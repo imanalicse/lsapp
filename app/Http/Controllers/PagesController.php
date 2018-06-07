023ea04d21d9f271b3dc36477a6c990ede5a41c0
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-06-07 21:09:58 +0600

    pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,13 +7,21 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $title = "Welcome to Laravel! 2";
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about() {
-        return view('pages.about');
+        $title = "About Us";
+        return view('pages.about')->with('title', $title);
     }
     public function services() {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web design', 'Programming', 'SEO']
+        );
+
+        return view('pages.services')->with($data);
     }
 }
