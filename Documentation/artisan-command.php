077fb27a8fceeb31470d91d1e19e04c6e6540905
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-04 23:55:57 +0600

    Post controller

--- Documentation/artisan-command.php
+++ Documentation/artisan-command.php
@@ -1,6 +1,6 @@
 <!-- Controller create command -->
 php artisan make:controller PagesController
-
+php artisan make:controller PostsController --resouce
 <!-- 
     Model create command -m for migration    
 -->
@@ -8,3 +8,9 @@ php artisan make:model Post -m
 
 <!-- Migration command -->
 php artisan migrate
+
+<!-- Tinker command -->
+
+php artisan tinker
+
+php artisan route:list
