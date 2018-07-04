4549d556fe41e89bc7813ab49ff903ec1fb2ad42
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-04 23:19:10 +0600

    migration

--- Documentation/artisan-command.php
+++ Documentation/artisan-command.php
@@ -1,2 +1,10 @@
 <!-- Controller create command -->
 php artisan make:controller PagesController
+
+<!-- 
+    Model create command -m for migration    
+-->
+php artisan make:model Post -m
+
+<!-- Migration command -->
+php artisan migrate
