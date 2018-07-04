1f3e74794d90183594031067b954739939075c71
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-04 22:40:45 +0600

    menu

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends("layouts.app")
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the laravel application from the 'laravel from scratch' </p>
+    <div class="jumbotron text-align-center">
+        <h1>{{$title}}</h1>
+        <p>This is the laravel application from the 'laravel from scratch' </p>
+        <p><a class="button btn-primary" href="/lsapp/login">Login</a> <a class="button btn-primary" href="/lsapp/register">Register</a> </p>
+    </div>
 @endsection
