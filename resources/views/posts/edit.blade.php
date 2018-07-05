9e921832ebbe63ea74f839cbe6d0005a299cc021
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-06 00:09:05 +0600

    post edit delet

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Edit post</h1>
+    {!! Form::open(['action' => ['PostsController@update', $post->id], 'method' => 'PUT']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', $post->title, ['class' => 'form-controll', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+                {{Form::label('body', 'Body')}}
+                {{Form::textarea('body', $post->body, ['id' => 'article-ckeditor', 'class' => 'form-controll', 'placeholder' => 'Body text'])}}
+        </div>
+        {{Form::hidden('_method', 'PUT')}}
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+        
+    {!! Form::close() !!}
+
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
     <a href="{{url('posts')}}" class="btn btn-default">Go back</a>
-    <h1>{{$post->title}}}</h1>
+    <h1>{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
     </div>
     <hr>
     <small> Written on {{$post->created_at}}}</small>
+    <hr>
+    <a href="{{url('posts/'.$post->id.'/edit')}}" class="btn btn-default">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class'=> 'pull-right']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection
