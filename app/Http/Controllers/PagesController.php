77954cd41ccb8cc589e43eeaff8eebb2b2a673e8
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-06 21:48:50 +0600

    home page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        $title = "Welcome to Laravel! 2";
+        $title = "Welcome to Laravel!";
         //return view('pages.index', compact('title'));
         return view('pages.index')->with('title', $title);
     }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,6 @@
     <div class="jumbotron text-align-center">
         <h1>{{$title}}</h1>
         <p>This is the laravel application from the 'laravel from scratch' </p>
-        <p><a class="button btn-primary" href="/lsapp/login">Login</a> <a class="button btn-primary" href="/lsapp/register">Register</a> </p>
+        <p><a class="btn btn-primary" href="{{url('login')}}">Login</a> <a class="btn btn-primary" href="{{url('register')}}">Register</a> </p>
     </div>
 @endsection
