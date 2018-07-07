d74bb45703adca7eb41fc3080f072f1dd9f0ca83
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-07-07 17:32:59 +0600

    style add

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron text-align-center">
         <h1>{{$title}}</h1>
-        <p>This is the laravel application from the 'laravel from scratch' </p>
+        <p class="test">This is the laravel application from the 'laravel from scratch' </p>
         <p><a class="btn btn-primary" href="{{url('login')}}">Login</a> <a class="btn btn-primary" href="{{url('register')}}">Register</a> </p>
     </div>
 @endsection
