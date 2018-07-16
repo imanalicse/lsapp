a768d81ffbc811df72eb3a35620933fb4f7037a6
iman <iman@bitmascot.com>
2018-07-16 14:37:13 +0600

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('posts')->insert([
+            'title' => 'Hello World',
+            'user_id' => 1,
+            'body' => '<strong>Hello world content</strong> Hello world content Hello world content Hello world content Hello world content ',            
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Android',
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+        ]);                
     }
 }
